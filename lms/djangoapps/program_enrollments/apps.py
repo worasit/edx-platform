cb8115e7768a9e35d752bf28c2149a5f5f7aea14
Rick Reilly <rreilly@edx.org>
2019-04-08 16:42:30 -0400

    Add ProgramEnrollment app and model for masters

---
+++ lms/djangoapps/program_enrollments/apps.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+ProgramEnrollments Application Configuration
+"""
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ProgramEnrollmentsConfig(AppConfig):
+    name = 'program_enrollments'
